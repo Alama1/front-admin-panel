9715fa895862a6721c19d01ec061beae5e70af1b
Maxim Kuzhdenko <oldpaladin7@gmail.com>
2024-05-23 23:27:44 +0300

    SignUp/SignIn done

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,6 +1,16 @@
 import { Fragment, useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
+import CheckIcon from '@mui/icons-material/Check';
+
 
 import meme from 'assets/meme.png'
 import triangle from 'assets/backgroundTriangle.png'
@@ -16,6 +26,10 @@ const Home = () => {
     const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
     const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
     const [hamstersFacts, setHamstersFacts] = useState([{title: 'Error', fact: 'Pls reload you page'}])
+    const [isSuccessAlertShown, setIsSuccessAlertShown] = useState(false)
+    const [isFailureAlertShown, setIsFailureAlertShown] = useState(false)
+    const [successAlertText, setSuccessAlertText] = useState('')
+    const [failureAlertText, setFailureAlertText] = useState('')
 
     const openLoginModal = () => {
       setIsLoginModalOpen(true);
@@ -30,20 +44,131 @@ const Home = () => {
         setIsSignUpModalOpen(false);
     };
   
+    async function handleRegistration(event) {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const email = data.get('email');
+        const password = data.get('password');
+        const username = data.get('username');
+        try {
+            const loginRes = await fetch('http://localhost:3001/signup', { 
+                method: 'POST', 
+                headers: {
+                    'Access-Control-Allow-Origin': 'http://localhost',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password, username }) 
+            });
+            const response = await loginRes.json()
+            
+            if (response.success) {
+                setIsLoggedIn(true)
+                localStorage.setItem('token', response.message.token)
+                setIsSuccessAlertShown(true)
+                setSuccessAlertText('Sign in success!')
+                setTimeout(() => {
+                    closeSignUpModal()
+                    setIsSuccessAlertShown(false)
+                }, 2000)
+            } else {
+                setIsFailureAlertShown(true)
+                setFailureAlertText(response.message)
+                setTimeout(() => {
+                    setIsFailureAlertShown(false)
+                }, 5000)
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    async function handleAuth(event) {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const email = data.get('email');
+        const password = data.get('password');
+    
+        try {
+            const loginRes = await fetch('http://localhost:3001/login', { 
+                method: 'POST', 
+                headers: {
+                    'Access-Control-Allow-Origin': 'http://localhost',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password }) 
+            });
+            const response = await loginRes.json()
+            
+            if (response.success) {
+                setIsLoggedIn(true)
+                localStorage.setItem('token', response.message.token)
+                setIsSuccessAlertShown(true)
+                setSuccessAlertText('Sign in success!')
+                setTimeout(() => {
+                    closeLoginModal()
+                    setIsSuccessAlertShown(false)
+                }, 2000)
+            } else {
+                setIsFailureAlertShown(true)
+                setFailureAlertText(response.message)
+                setTimeout(() => {
+                    setIsFailureAlertShown(false)
+                }, 5000)
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     const modalStyle = {
         position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 400,
+        width: 500,
         bgcolor: 'background.paper',
         border: '2px solid #000',
+        borderRadius: '8px',
         boxShadow: 24,
         pt: 2,
         px: 4,
         pb: 3,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+
       };
+
+    async function updateToken(token) {
+        const updateRes = await fetch('http://localhost:3001/updateToken', { 
+            method: 'GET', 
+            headers: {
+                'Access-Control-Allow-Origin': 'http://localhost',
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${token}`
+
+            }
+        });
+        const response = await updateRes.json()
+        
+        if (response.message === 'Token verification failed.') {
+            localStorage.removeItem('token')
+            return
+        }
+        if (response.message === 'Invalid token.') {
+            localStorage.removeItem('token')
+            return
+        }
+        if (response.message === 'Only tokens with less than 1 day lifetime can be extended.') {
+            setIsLoggedIn(true)
+            return
+        }
+        if (response.success) {
+            setIsLoggedIn(true)
+            localStorage.setItem('token', response.message.token)
+        }
+    }
 
     useEffect(() => {
         setHamstersFacts([
@@ -54,6 +179,13 @@ const Home = () => {
             {title: 'Title 5', fact: 'Fact 5'},
             //TODO API
         ])
+    }, [])
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            updateToken(token)
+        }
     }, [])
 
     return (
@@ -114,12 +246,65 @@ const Home = () => {
                 onClose={closeLoginModal}
                 aria-labelledby="parent-modal-title"
                 aria-describedby="parent-modal-description"
+                className='login-modal'
             > 
                 <Box sx={{ ...modalStyle }}>
-                <h2 id="parent-modal-title">Login</h2>
-                <p id="parent-modal-description">
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </p>
+                    <Typography component="h1" variant="h5">
+                        Sign In
+                    </Typography>
+                    {
+                        isSuccessAlertShown ?
+                        <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+                            {successAlertText}
+                        </Alert> : null
+                    }
+                    {
+                        isFailureAlertShown ?
+                        <Alert severity="error">
+                            {failureAlertText}
+                        </Alert> : null
+                    }
+                    <Box component="form" noValidate onSubmit={handleAuth} sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
                 </Box>
             </Modal>
             <Modal
@@ -129,10 +314,64 @@ const Home = () => {
                 aria-describedby="parent-modal-description"
             > 
                 <Box sx={{ ...modalStyle }}>
-                <h2 id="parent-modal-title">SignUp</h2>
-                <p id="parent-modal-description">
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </p>
+                        <Typography component="h1" variant="h5">
+                                Sign up
+                        </Typography>
+                        {
+                            isSuccessAlertShown ?
+                            <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+                                {successAlertText}
+                            </Alert> : null
+                        }
+                        {
+                            isFailureAlertShown ?
+                            <Alert severity="error">
+                                {failureAlertText}
+                            </Alert> : null
+                        }
+                        <Box component='form' onSubmit={handleRegistration} sx={{mt: 3}}>
+                            <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Username"
+                                name="username"
+                                autoComplete="family-name"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                />
+                            </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    />
+                                </Grid> 
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                >
+                                Sign Up
+                            </Button>
+                        </Box>
                 </Box>
             </Modal>
         </Fragment>
